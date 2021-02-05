2d4d0bc28c93da785b8e950b5addfc5dd512dfc1
ruanheng <ruanheng159@163.com>
2021-02-06 07:53:21 +0800

    feat(id4):新增资源所有者密码验证Demo

---
+++ 2_ResourceCredentials/IdentityServer/Config.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("orderApi", "Order Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "orderApi" }
+                },
+                new Client()
+                {
+                    ClientId = "WinForm",
+                    ClientSecrets =
+                    {
+                        new Secret("winform_secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = {"orderApi"},
+                    AllowOfflineAccess = true, //允许返回Refresh Token
+                }
+            };
+        }
+
+        public static List<TestUser> GeTestUsers()
+        {
+            return  new List<TestUser>
+            {
+                new TestUser()
+                {
+                    SubjectId = "1",
+                    Username = "Zoe",
+                    Password = "123456"
+                },
+                new TestUser()
+                {
+                    SubjectId = "2",
+                    Username = "Ran",
+                    Password = "111222"
+                }
+            };
+        }
+    };
+}
---
+++ 2_ResourceCredentials/WinformClient/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IdentityModel;
+using IdentityModel.Client;
+
+namespace WinformClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string username = this.textBox1.Text.ToString();
+            string password = this.textBox2.Text.ToString();
+
+            HttpClient client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:6000");
+
+            if (disco.IsError)
+            {
+                MessageBox.Show(disco.Error);
+                return;
+            }
+
+            var tokenResp = await client.RequestPasswordTokenAsync(new PasswordTokenRequest()
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "WinForm",
+                ClientSecret = "winform_secret",
+                UserName = username,
+                Password = password,
+            });
+            if (tokenResp.IsError)
+            {
+                MessageBox.Show(tokenResp.Error);
+                return;
+            }
+
+            this.textBox3.Text = tokenResp.Json.ToString();
+            if (!tokenResp.IsError)
+            {
+                accessToken = tokenResp.AccessToken;
+            }
+        }
+
+        private string accessToken = string.Empty;
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                MessageBox.Show("请先获取Token");
+                return;
+            }
+
+            HttpClient client = new HttpClient();
+
+            client.SetBearerToken(accessToken);
+            var contentResp = await client.GetAsync("http://localhost:5000/api/order");
+
+            if (contentResp.IsSuccessStatusCode)
+            {
+                var content = await contentResp.Content.ReadAsStringAsync();
+                this.textBox4.Text = content;
+            }
+            else
+            {
+                MessageBox.Show(contentResp.StatusCode.ToString());
+                return;
+            }
+        }
+    }
+}
